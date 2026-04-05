deb0b4f3f06f896efa679706e9bc5f77fb13a538
Parmeet <parmeetb2002@gmail.com>
2026-04-05 15:44:56 +0530

    Update Render build script to include Vite frontend build.

--- render-build.sh
+++ render-build.sh
@@ -5,6 +5,12 @@ set -o errexit
 # Install python dependencies
 pip install -r requirements.txt
 
+# Build the frontend (Vite)
+cd civic_ui
+npm install
+npm run build
+cd ..
+
 # Run migrations
 python manage.py migrate
 
